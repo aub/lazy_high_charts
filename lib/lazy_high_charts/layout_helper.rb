b0b2e2c1a25366c0b7f07e21d627ad3f59a66f32
Jon Pospischil <pospischil@gmail.com>
2012-03-28 01:23:23 -0400

    instead of json'ing an entire hash, json each k/v individually (recursively)

--- lib/lazy_high_charts/layout_helper.rb
+++ lib/lazy_high_charts/layout_helper.rb
@@ -22,15 +22,62 @@ module LazyHighCharts
       build_html_output("StockChart", placeholder, object, &block)
     end
 
+
+    def build_html_output(type, placeholder, object, formatter=nil)
+      #old way is commented out
+      #options_collection = []
+      #object.options.keys.each do |key|
+      #  k = key.to_s.camelize.gsub!(/\b\w/) { $&.downcase }
+      #  options_collection << "#{k}: #{object.options[key].to_json}"
+      #end
+      options_collection = jsonify_hash_with_exceptions(object.options, ["onclick"])
+      options_collection << "series: #{object.data.to_json}"
+
+      graph =<<-EOJS
+      <script type="text/javascript">
+      (function() {
+        var onload = window.onload;
+        window.onload = function(){
+          if (typeof onload == "function") onload();
+          var options, chart;
+          options = { #{options_collection.join(",")} };
+          #{formatter if formatter}
+          chart = new Highcharts.#{type}(options);
+        };
+      })()
+      </script>
+      EOJS
+
+      if defined?(raw)
+        return raw(graph) 
+      else
+        return graph
+      end
+
+    end
+    def jsonify_hash_with_exceptions(hash, exceptions)
+      options_collection = []
+      hash.keys.each do |key|
+        k = key.to_s.camelize.gsub!(/\b\w/) { $&.downcase }
+        if hash[key].is_a? Hash
+          options_collection << "#{k}: {#{jsonify_hash_with_exceptions(hash[key], exceptions).join(", ")}}"
+        else
+          if exceptions.include?(k)
+            options_collection << "#{k}: #{hash[key]}"
+          else
+            options_collection << "#{k}: #{hash[key].to_json}"
+          end
+        end
+      end
+      options_collection
+    end
+
+
     def build_html_output(type, placeholder, object, formatter=nil)
       options_collection = []
       object.options.keys.each do |key|
         k = key.to_s.camelize.gsub!(/\b\w/) { $&.downcase }
-        if k == "onclick"
-          options_collection << "#{k}: #{object.options[key]}"
-        else
-          options_collection << "#{k}: #{object.options[key].to_json}"
-        end
+        options_collection << "#{k}: #{object.options[key].to_json}"
       end
       options_collection << "series: #{object.data.to_json}"
 
