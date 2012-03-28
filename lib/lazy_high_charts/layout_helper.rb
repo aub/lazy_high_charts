8697d35795f9f1f6e1653017aa7c88b2741a277e
Jon Pospischil <pospischil@gmail.com>
2012-03-27 23:10:47 -0400

    old style formatter param

--- lib/lazy_high_charts/layout_helper.rb
+++ lib/lazy_high_charts/layout_helper.rb
@@ -2,27 +2,27 @@
 module LazyHighCharts
   module LayoutHelper
 
-    def high_chart(placeholder, object  , &block)
+    def high_chart(placeholder, object  , formatter=nil)
       object.html_options.merge!({:id=>placeholder})
       object.options[:chart][:renderTo] = placeholder
-      high_graph(placeholder,object , &block)#.concat(content_tag("div","", object.html_options))
+      high_graph(placeholder,object , formatter)#.concat(content_tag("div","", object.html_options))
     end
 
-    def high_stock(placeholder, object  , &block)
+    def high_stock(placeholder, object  , formatter=nil)
       object.html_options.merge!({:id=>placeholder})
       object.options[:chart][:renderTo] = placeholder
       high_graph_stock(placeholder,object , &block).concat(content_tag("div","", object.html_options))
     end
 
-    def high_graph(placeholder, object, &block)
-      build_html_output("Chart", placeholder, object, &block)
+    def high_graph(placeholder, object, formatter=nil)
+      build_html_output("Chart", placeholder, object, formatter)
     end
 
-    def high_graph_stock(placeholder, object, &block)
+    def high_graph_stock(placeholder, object, formatter=nil)
       build_html_output("StockChart", placeholder, object, &block)
     end
 
-    def build_html_output(type, placeholder, object, &block)
+    def build_html_output(type, placeholder, object, formatter=nil)
       options_collection = []
       object.options.keys.each do |key|
         k = key.to_s.camelize.gsub!(/\b\w/) { $&.downcase }
@@ -38,7 +38,7 @@ module LazyHighCharts
           if (typeof onload == "function") onload();
           var options, chart;
           options = { #{options_collection.join(",")} };
-          #{capture(&block) if block_given?}
+          #{formatter if formatter}
           chart = new Highcharts.#{type}(options);
         };
       })()
