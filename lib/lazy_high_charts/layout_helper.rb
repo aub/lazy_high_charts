5188d38e745066eb0c79eef84e22ccff803f70c1
Jon Pospischil <pospischil@gmail.com>
2012-03-28 01:29:48 -0400

    remove duplicate code

--- lib/lazy_high_charts/layout_helper.rb
+++ lib/lazy_high_charts/layout_helper.rb
@@ -22,14 +22,7 @@ module LazyHighCharts
       build_html_output("StockChart", placeholder, object, &block)
     end
 
-
     def build_html_output(type, placeholder, object, formatter=nil)
-      #old way is commented out
-      #options_collection = []
-      #object.options.keys.each do |key|
-      #  k = key.to_s.camelize.gsub!(/\b\w/) { $&.downcase }
-      #  options_collection << "#{k}: #{object.options[key].to_json}"
-      #end
       options_collection = jsonify_hash_with_exceptions(object.options, ["onclick"])
       options_collection << "series: #{object.data.to_json}"
 
@@ -55,6 +48,7 @@ module LazyHighCharts
       end
 
     end
+
     def jsonify_hash_with_exceptions(hash, exceptions)
       options_collection = []
       hash.keys.each do |key|
@@ -70,38 +64,6 @@ module LazyHighCharts
         end
       end
       options_collection
-    end
-
-
-    def build_html_output(type, placeholder, object, formatter=nil)
-      options_collection = []
-      object.options.keys.each do |key|
-        k = key.to_s.camelize.gsub!(/\b\w/) { $&.downcase }
-        options_collection << "#{k}: #{object.options[key].to_json}"
-      end
-      options_collection << "series: #{object.data.to_json}"
-
-      graph =<<-EOJS
-      <script type="text/javascript">
-      (function() {
-        var onload = window.onload;
-        window.onload = function(){
-          if (typeof onload == "function") onload();
-          var options, chart;
-          options = { #{options_collection.join(",")} };
-          #{formatter if formatter}
-          chart = new Highcharts.#{type}(options);
-        };
-      })()
-      </script>
-      EOJS
-
-      if defined?(raw)
-        return raw(graph) 
-      else
-        return graph
-      end
-
     end
   end
 end
